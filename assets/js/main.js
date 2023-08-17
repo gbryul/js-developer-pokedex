f33686d65255e4ad17302d82acb4b64eab9d1f6c
gbryul <gabriel.unionjack@gmail.com>
2023-08-17 16:38:33 -0300

    detalhes finais

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,7 @@ const maxRecords = 151
 const limit = 10
 let offset = 0;
 
-function loadDetails(number){
-    const moreDetails = []
-    moreDetails[number] = document.getElementById(`moreDetails${number}`)
 
-
-        if(moreDetails[number].style.display == "block"){
-            moreDetails[number].style.display = "none"
-        }
-        else
-        {
-            moreDetails[number].style.display = "block"
-        }
-}
 
 function convertPokemonToLi(pokemon) {
     return `
--- assets/js/more-details.js
+++ assets/js/more-details.js
@@ -1,1 +1,15 @@
 
+
+function loadDetails(number){
+    const moreDetails = []
+    moreDetails[number] = document.getElementById(`moreDetails${number}`)
+
+
+        if(moreDetails[number].style.display == "block"){
+            moreDetails[number].style.display = "none"
+        }
+        else
+        {
+            moreDetails[number].style.display = "block"
+        }
+}
